96b97c371f42995e455677c93f8c817d28442523
shicks <shicks255@yahoo.com>
2021-01-19 22:29:14 -0600

    More slow progress

--- app/src/main/java/com/steven/metronome/MainActivity.kt
+++ app/src/main/java/com/steven/metronome/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.steven.metronome
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.steven.metronome.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
@@ -11,5 +11,13 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        requireNotNull(this.application)
+
+        val viewModelFactory = MainViewModelFactory(this.application)
+        val viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+
+        binding.viewModel = viewModel
+        binding.setLifecycleOwner(this)
     }
 }
--- app/src/main/java/com/steven/metronome/MainViewModel.kt
+++ app/src/main/java/com/steven/metronome/MainViewModel.kt
@@ -2,7 +2,37 @@ package com.steven.metronome
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
 
+    val _tempo = MutableLiveData<Int>()
+    val tempo: LiveData<Int>
+        get() = _tempo
+
+    val _enabled = MutableLiveData<Boolean>()
+    val enabled:  LiveData<Boolean>
+        get() = _enabled
+
+    init {
+        _tempo.value = 120
+        _enabled.value = false
+    }
+
+    fun setTempo(value: Int?) {
+        _tempo.value = value
+    }
+
+    fun increaseTempo() {
+        _tempo.value = _tempo.value?.plus(1)
+    }
+
+    fun decreaseTempo() {
+        _tempo.value = _tempo.value?.minus(1)
+    }
+
+    fun toggleEnabled() {
+        _enabled.value = !_enabled.value!!
+    }
 }
