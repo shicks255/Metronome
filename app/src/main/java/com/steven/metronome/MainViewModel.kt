c14e89f86d0653874aeb6c142701b69709c139c0
shicks <shicks255@yahoo.com>
2021-01-20 22:14:34 -0600

    Tapping tempo functionality

--- app/src/main/java/com/steven/metronome/MainViewModel.kt
+++ app/src/main/java/com/steven/metronome/MainViewModel.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import java.time.Duration
+import java.time.LocalTime
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
 
@@ -15,9 +17,31 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
     val enabled:  LiveData<Boolean>
         get() = _enabled
 
+    val _tapStart = MutableLiveData<LocalTime>()
+    val tapStart: LiveData<LocalTime>
+        get() = _tapStart
+
     init {
         _tempo.value = 120
         _enabled.value = false
+        _tapStart.value = null
+    }
+
+    /*
+     * Tap tempo algorith:
+     * Duration in ms between 2 taps
+     * 60_000 ms / Duration
+     */
+    fun tapTempo() {
+        if (_tapStart.value != null) {
+            val durationMillis = Duration.between(_tapStart.value, LocalTime.now()).toMillis()
+            if (durationMillis < 1000*120) { // only if the tap are within 2 minutes of eachother, should probably adjust thig
+                val newTempo = 60_000 / durationMillis
+                setTempo(newTempo.toInt())
+            }
+        }
+
+        _tapStart.value = LocalTime.now()
     }
 
     fun setTempo(value: Int?) {
